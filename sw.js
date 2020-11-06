10bb4ff9244e030262553cdc5ddae3da6f26ef64
R4356th <R4356th@users.noreply.github.com>
2020-11-06 18:08:26 +0000

    Prettier Formatted

--- sw.js
+++ sw.js
@@ -1,9 +1,5 @@
 const staticCacheName = "Glitchy Scratch";
-const assets = [
-  "Glitchy Scratch_files/",
-  "svgs",
-  "Scratch - Community Blocks Beta Tester Interviews_files"
-];
+const assets = ["Glitchy Scratch_files/", "svgs", "Scratch - Community Blocks Beta Tester Interviews_files"];
 
 self.addEventListener("install", (evt) => {
   evt.waitUntil(
@@ -17,11 +13,7 @@ self.addEventListener("install", (evt) => {
 self.addEventListener("activate", (evt) => {
   evt.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys
-          .filter((key) => key !== staticCacheName)
-          .map((key) => caches.delete(key))
-      );
+      return Promise.all(keys.filter((key) => key !== staticCacheName).map((key) => caches.delete(key)));
     })
   );
 });
